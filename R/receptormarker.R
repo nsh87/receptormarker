bd459eeab5d48ff12d62bcddca48c13f7ee5a4a9
Nikhil Haas <nikhil@nikhilhaas.com>
2015-05-21 00:40:27 -0700

    Remove unneeded doc files and generate package doc file

---
+++ R/receptormarker.R
@@ -0,0 +1,7 @@
+#' Analyze antibody receptor sequences and phenotypic markers.
+#'
+#' Useful for quick normalization, clustering, and creating phylogenetic trees.
+#'
+#' @docType package
+#' @name receptormarker
+NULL
