60c61451775fe1f575a0c0a5547eb3d3125f2b72
Nikhil Haas <nikhil@nikhilhaas.com>
2015-05-24 23:39:55 -0700

    Change custom Travis CI build Rscript to shell script   - .sh runs Rscript to do devtools::check() and ::test(),     then greps the results and exits with 1 status code if     "ERROR", "WARNING", and "Failure" found in the output     of the Rscript. This is a workaround to devtools::check()     and devtools::test() not exiting with a non-zero status     code if the check or test case fails; Travis needs     a non-zero status code to fail the build.

---
+++ travis_run_build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+log_file="travis_build.log"
+#R CMD BATCH ./travis_build.R $log_file 
+#cat $log_file
+
+# touch $log_file
+# 
+# # run tail -f in background
+# tail -f $log_file # > out 2>&1 &
+# 
+# # process id of tail command
+# tailpid=$!
+# 
+# # run build
+# R CMD BATCH ./travis_build.R $log_file 
+# 
+# # now kill the tail process
+# kill $tailpid
+
+# run checks
+eval "Rscript ./travis_build.R | tee -a '$log_file'"
+
+# search for errors
+err1="ERROR"
+err2="WARNING"
+err3="Failure (at"
+err4="Failure(@"
+if ! grep -q "$err1\|$err2\|$err3\|$err4" $log_file; then
+    echo "No errors, warnings, or failures found."
+else 
+    echo "*** grep results **********************"
+    grep -n "$err1" $log_file
+    grep -n "$err2" $log_file
+    grep -n "$err3" $log_file
+    grep -n "$err4" $log_file
+    echo "ERROR, WARNING, or Failure found. See grep results above."
+    exit 1
+fi
