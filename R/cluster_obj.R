4000c6a0f142e383311ddb52821411caea21d697
catterbu <cbatterbury@dons.usfca.edu>
2015-06-05 00:41:41 -0700

    Initial cluster object commit   - Add initial parameters   - Add Roxygen skeleton

---
+++ R/cluster_obj.R
@@ -0,0 +1,15 @@
+#' Title
+#'
+#' @param data 
+#' @param krange 
+#' @param iter.max 
+#' @param runs 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cluster_obj <- function(data, krange = 2:10, iter.max = 100, runs = 100, ...) {
+  
+}
