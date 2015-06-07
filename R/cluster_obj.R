3843533532925b46d7febac29ce4e39f8dba595c
catterbu <cbatterbury@dons.usfca.edu>
2015-06-07 00:16:52 -0700

    Start cluster object   - Add import cluster   - Add check that krange does not include 1   - Create distance object from data   - Write lapply for km

--- R/cluster_obj.R
+++ R/cluster_obj.R
@@ -7,9 +7,30 @@
 #' @param ... 
 #'
 #' @return
+#' 
+#' @import cluster
+#' 
 #' @export
 #'
 #' @examples
-cluster_obj <- function(data, krange = 2:10, iter.max = 100, runs = 100, ...) {
+cluster_obj <- function(data, krange = 2:10, iter.max = 100, runs=100, ...) {
+  if (1 %in% krange) stop("The entire range for # of clusters is to be > 1.")
+  data_dist <- dist(data)
   
+  km <- lapply(krange, 
+               function(k) {
+                 sil_max <- 0
+                 km_opt <- NULL
+                 for (i in 1:runs) {
+                   kmm <- kmeans(data, k, iter.max = iter.max, ...)
+                   sil_sum <- summary(silhouette(kmm$cluster, data_dist))
+                   sil_avg <- sil_sum$avg.width
+                   if (sil_avg > sil_max) {
+                     sil_max <- sil_avg
+                     km_opt <- kmm
+                   }
+                 }
+                 list(km_obj = km_opt, sil_avg = sil_max)
+               })
+  k_best <- which.max(km$sil_avg)
 }
