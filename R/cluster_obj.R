4cf311e30d23a015e817d1d9779dc3620fd6ac26
catterbu <cbatterbury@dons.usfca.edu>
2015-06-07 23:31:03 -0700

    Finish initial clust_obj documentation   - Complete description   - Add return section   - Add seealso section   - Add examples section

--- R/cluster_obj.R
+++ R/cluster_obj.R
@@ -1,9 +1,9 @@
 #' Cluster with kmeans and find optimal k
 #' 
 #' This calls the function \code{\link{kmeans}} to perform kmeans clustering,
-#' but initializes multiple times and chooses the best one for each number of
-#' clusters based on average silhouette width using the \code{\link{silhouette}}
-#' 
+#' but initializes multiple times. It chooses the best one for each number of
+#' clusters, and the best number of clusters overall, based on average 
+#' silhouette width using the \code{\link{silhouette}} function.
 #'
 #' @param data A numeric matrix of data, or an object that can be coerced to
 #'   such a matrix (such as a numeric vector or a data frame with all numeric
@@ -14,13 +14,27 @@
 #' @param runs An integer. Number of starts of the k-means algorithm.
 #' @param ... further arguments to be passed to \code{\link{kmeans}}.
 #'
-#' @return 
+#' @return \code{cluster_obj} returns an object of class "cluster_obj" that can
+#'   be used for multiple plots. It is a list with at least the following
+#'   components:
+#'   \code{km_obj} A list of \code{\link{kmeans}} objects for each number of
+#'     cluster centers requested.
+#'   \code{sil_avg} A list of average silhouette scores for each number of
+#'     cluster centers requested.
+#'   \code{num_clusters} A list of the each number of clusters used.
+#'   \code{sil} A list of \code{\link{silhouette}} objects for each number of
+#'     clusters.
+#'   \code{k_best} The optimal number of clusters based on silhouette score.
 #' 
 #' @import cluster
 #' 
 #' @export
 #'
+#' @seealso \code{\link{kmeans}}, \code{\link{silhouette}}
+#'
 #' @examples
+#' library(datasets)
+#' iris_cluster <- cluster_obj(iris[, 1:4])
 cluster_obj <- function(data, krange = 2:10, iter.max = 100, runs=100, ...) {
   if (1 %in% krange) stop("The entire range for # of clusters is to be > 1.")
   data_dist <- dist(data)
