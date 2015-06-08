378790fddcd3600611caa1134c5df36c5f2aec2c
catterbu <cbatterbury@dons.usfca.edu>
2015-06-08 00:14:46 -0700

    Initial cluster plotting commit   - Add wss_plot initial parameters   - Add wss_plot roxygen skeleton

---
+++ R/cluster_plotting.R
@@ -0,0 +1,14 @@
+#' Plot of total within sum of squares for numbers of clusters
+#' 
+#' This function plots.
+#'
+#' @param clust_obj 
+#' @param optimal 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+wss_plot <- function(clust_obj, optimal = FALSE) {
+  
+}
