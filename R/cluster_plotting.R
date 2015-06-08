89e9105417840caf90a85d8ee2b465301a550d14
catterbu <cbatterbury@dons.usfca.edu>
2015-06-08 01:08:53 -0700

    Add code to generate plot   - Create wss using vapply   - Plot cluster wss   - Add conditional for optimal clustering

--- R/cluster_plotting.R
+++ R/cluster_plotting.R
@@ -10,5 +10,22 @@
 #'
 #' @examples
 wss_plot <- function(clust_obj, optimal = FALSE) {
-  
+  krange <- 1:length(clust_obj$clust_model)
+  wss <- vapply(krange, 
+                function(k) {
+                  if (is.na(clust_obj$clust_model[[k]])) {
+                    wss <- 0
+                  } else {
+                    wss <- sum(clust_obj$clust_model[[k]]$withinss)
+                  }
+                  wss
+                },
+                numeric(1))
+  plot(krange, wss,type="b", xlab="# of Clusters", ylab="Within Sum of Squares",
+       main="Within Sum of Squares by Cluster")
+  if (optimal) {
+    opti_clust <- clust_obj$k_best
+    points(opti_clust, wss[opti_clust], col = "red", pch = 1, cex = 3)
+    legend("topright", "Optimal Clusters", col = "red", pch = 1)
+  }
 }
