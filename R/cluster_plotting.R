cafa3b2c1cdeb74d12860a9d16d29c68dc471d91
catterbu <cbatterbury@dons.usfca.edu>
2015-06-13 23:26:13 -0700

    Add ellipsis argument to plot functions to be passed to plot

--- R/cluster_plotting.R
+++ R/cluster_plotting.R
@@ -4,12 +4,14 @@
 #'
 #' @param clust_obj 
 #' @param optimal 
+#' @param ... Additional arguments to be passed to the \code{\link{plot}} 
+#'   function (besides \code{xlab}, \code{ylab}, \code{main}).
 #'
 #' @return
 #' @export
 #'
 #' @examples
-wss_plot <- function(clust_obj, optimal = FALSE) {
+wss_plot <- function(clust_obj, optimal = FALSE, ...) {
   wss <- clust_obj['wss']
   start <- length(wss[is.na(wss)]) + 1 # We start the krange right after NA's
   end <- length(wss)
@@ -17,7 +19,8 @@ wss_plot <- function(clust_obj, optimal = FALSE) {
   wss <- wss[!is.na(wss)]
   plot(krange, wss, type = "b", xlab = "# of Clusters", 
        ylab = "Within Sum of Squares", 
-       main = "Within Sum of Squares by Cluster")
+       main = "Within Sum of Squares by Cluster",
+       ...)
   if (optimal) {
     opti_clust <- clust_obj['k_best'] - start # Accounts for removal of NA's
     points(opti_clust, wss[opti_clust], col = "red", pch = 1, cex = 3)
@@ -31,13 +34,16 @@ wss_plot <- function(clust_obj, optimal = FALSE) {
 #'
 #' @param clust_obj 
 #' @param optimal 
+#' @param ... Additional arguments to be passed to the \code{\link{plot}} 
+#'   function (besides \code{xlab}, \code{ylab}, \code{main}).
 #'
 #' @return
 #' @export
 #'
 #' @examples
-clusGap_plot <- function(clust_obj, optimal = FALSE) {
-  plot(clust_obj['clust_gap'], xlab = "# of Clusters", main = "Gap Analysis")
+clusGap_plot <- function(clust_obj, optimal = FALSE, ...) {
+  plot(clust_obj['clust_gap'], xlab = "# of Clusters", main = "Gap Analysis",
+       ...)
   if (optimal) {
     opti_clust <- clust_obj['k_best']
     gap_best <- clust_obj['clust_gap']['Tab'][[1]][opti_clust, 3]
@@ -54,18 +60,21 @@ clusGap_plot <- function(clust_obj, optimal = FALSE) {
 #' @param data 
 #' @param clust_obj 
 #' @param num_clust 
+#' @param ... Additional arguments to be passed to the \code{\link{plot}} 
+#'   function (besides \code{xlab}, \code{ylab}, \code{main}).
 #'
 #' @return
 #' @export
 #'
 #' @examples
-pca_plot <- function(data, clust_obj, num_clust) {
+pca_plot <- function(data, clust_obj, num_clust, ...) {
   pca <- princomp(data)
   clusters <- clust_obj['clust_model'][[num_clust]]['cluster']
   plot(pca$scores[,1:2], col = rainbow(num_clust)[clusters],
        xlab = "Principal Component 1",
        ylab = "Principal Component 2",
-       main = "PCA Plot of Clusters")
+       main = "PCA Plot of Clusters",
+       ...)
 }
 
 #' Plot silhouette scores for a given clustering of data.
@@ -76,14 +85,16 @@ pca_plot <- function(data, clust_obj, num_clust) {
 #'
 #' @param clust_obj 
 #' @param num_clust 
+#' @param ... Additional arguments to be passed to the \code{\link{plot}} 
+#'   function (besides \code{xlab}, \code{ylab}, \code{main}).
 #'
 #' @return
 #' @export
 #'
 #' @examples
-sil_plot <- function(clust_obj, num_clust) {
+sil_plot <- function(clust_obj, num_clust, ...) {
   sil <- clust_obj['sil'][[num_clust]]
-  plot(sil)
+  plot(sil, ...)
 }
 
 #' Plot average silhouette widths for different numbers of clusters.
@@ -93,12 +104,14 @@ sil_plot <- function(clust_obj, num_clust) {
 #'
 #' @param clust_obj 
 #' @param optimal 
+#' @param ... Additional arguments to be passed to the \code{\link{plot}}
+#'   function (besides \code{xlab}, \code{ylab}, \code{main}).
 #'
 #' @return
 #' @export
 #'
 #' @examples
-avg_sil_plot <- function(clust_obj, optimal = FALSE) {
+avg_sil_plot <- function(clust_obj, optimal = FALSE, ...) {
   sil <- clust_obj['sil_avg']
   start <- length(sil[is.na(sil)]) + 1 # We start the krange right after NA's
   end <- length(sil)
@@ -106,10 +119,36 @@ avg_sil_plot <- function(clust_obj, optimal = FALSE) {
   sil <- sil[!is.na(sil)]
   plot(krange, sil, type = "b", xlab = "# of Clusters",
        ylab = "Average Silhouette Width", 
-       main = "Average Silhouette Width by Cluster")
+       main = "Average Silhouette Width by Cluster",
+       ...)
   if (optimal) {
     opti_clust <- clust_obj['k_best'] - start # Accounts for removal of NA's
     points(opti_clust, sil[opti_clust], col = "red", pch = 1, cex = 3)
     legend("bottomright", "Optimal Clusters", col = "red", pch = 1)
   }
+}
+
+#' Plot cluster membership for each feature.
+#' 
+#' This function plots cluster membership for each feature of \code{data} using
+#' box plots. 
+#'
+#' @param data 
+#' @param clust_obj 
+#' @param num_clust 
+#' @param ... Additional arguments to be passed to the \code{\link{qplot}} 
+#'   function (besides \code{xlab}, \code{ylab}).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clust_boxplot <- function(data, clust_obj, num_clust, ...) {
+  meas_vars <- colnames(data)
+  data['cluster'] <- clust_obj['clust_model'][[num_clust]]['cluster']
+  m <- melt(data, id.vars = "cluster", measure.vars = meas_vars)
+  qplot(x = as.factor(cluster), y = value, data = m, geom = "boxplot", 
+        fill = as.factor(cluster), xlab = NULL, 
+        ylab = "Relative expression level", ...) + 
+    facet_wrap(~variable) + scale_fill_discrete(name = "Cluster")
 }
