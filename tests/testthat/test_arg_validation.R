3f30c03c6f4ae38ae767a067698fe50446fbc997
catterbu <cbatterbury@dons.usfca.edu>
2015-07-01 14:11:01 -0700

    Fix data.frame not being used in arg validation test   - Fixes #21

--- tests/testthat/test_arg_validation.R
+++ tests/testthat/test_arg_validation.R
@@ -18,7 +18,7 @@ test_that("making sure arguments are not NULL works properly", {
   arg_list <- list(fake_param=NULL, another_param=42)
   expect_error(validate_not_null(arg_list), "fake_param")
   arg_list <- list(fake_param=TRUE, another_param=23, third_param=c(1:10),
-                   data_frame=c(5:15), fifth_param="test characters",
-                   sixth_param=NA)
+                   data_frame=data.frame(x=1, y=1:10, let="abc"),
+                   fifth_param="test characters", sixth_param=NA)
   expect_that(validate_not_null(arg_list), not(throws_error()))
 })
