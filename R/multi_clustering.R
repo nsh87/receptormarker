e019f370f0d9987cfec91449fd79c1f7e4cf67d2
catterbu <cbatterbury@dons.usfca.edu>
2015-07-25 19:00:42 -0700

    Add code for NbClust from pull request   - Include GPL-2 license   - Use formatR to tidy code   - Finish tidying code manually   - Add roxygen documentation   - Build Rd files for NbClust   - Modify multi_clust to use all methods in NbClust

--- R/multi_clustering.R
+++ R/multi_clustering.R
@@ -123,7 +123,7 @@ multi_clust <- function(d, krange = 2:15, iter.max = 300, runs = 10,
   km[["clust_gap"]] <- cluster::clusGap(d, kmeans, 
                                         K.max = length(km[["clust_model"]]), 
                                         B = 15, verbose = FALSE)
-  nb_best <- NbClust::NbClust(d, min.nc = krange[1],
+  nb_best <- NbClust::NbClust(d, min.nc = krange[1], index = "alllong",
                               max.nc = krange[length(krange)],
                               method = "average")
   best <- aggregate(nb_best$Best.nc[1,], by=list(nb_best$Best.nc[1,]), length)
