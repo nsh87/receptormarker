f164eb0a6a3a8673c93951a7fdca117aff17b7ac
catterbu <cbatterbury@dons.usfca.edu>
2016-04-05 22:37:41 -0700

    WIP - Optimize Dis function

--- R/NbClust.R
+++ R/NbClust.R
@@ -285,19 +285,14 @@ NbClust <- function(data = NULL, diss = NULL, distance = "euclidean",
     x <- as.matrix(x)
     k <- max(cl)
     centers.matrix <- centers(cl, x)
-    Distance.centers <- dist(centers.matrix)
+    Distance.centers <- as.matrix(dist(centers.matrix))
     Dmin <- min(Distance.centers)
     Dmax <- max(Distance.centers)
-    Distance.centers <- as.matrix(Distance.centers)
     s2 <- 0
     for (u in 1:k) {
-      s1 <- 0
-      for (j in 1:ncol(Distance.centers)) {
-        s1 <- s1 + Distance.centers[u, j]
-      }
-      s2 <- s2 + 1 / s1
+      s2 <- s2 + 1 / sum(Distance.centers[u, ])
     }
-    Dis <- (Dmax / Dmin) * s2
+    Dis <- Dmax / Dmin * s2
     return(Dis)
   }
   
