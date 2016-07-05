073e4c88412b47ba3a2cd64d36b2eb711bff9ff0
catterbu <cbatterbury@dons.usfca.edu>
2016-07-04 19:04:58 -0700

    Optimize varwithinss function

--- R/NbClust.R
+++ R/NbClust.R
@@ -667,10 +667,8 @@ NbClust <- function(data = NULL, diss = NULL, distance = "euclidean",
       nvar <- dim(x)[2]
       varwithins <- matrix(0, nrow, nvar)
       x <- (x - centers[cluster, ]) ^ 2
-      for (l in 1:nvar) {
-        for (k in 1:nrow) {
-          varwithins[k, l] <- sum(x[cluster == k, l])
-        }
+      for (k in 1:nrow) {
+        varwithins[k, ] <- colSums(x[cluster == k, ])
       }
       return(varwithins)
     }
